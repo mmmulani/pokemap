061b2f68af7faddc71986b46b20799252ebf45d4
Mehdi Mulani <mehdi@fb.com>
2014-10-29 18:32:20 -0400

    Rendering of tiles!

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 nlzss taken from https://github.com/magical/nlzss
+
+# Installing pygame
+
+`pygame` is annoying to install for Python 3.
+The easiest way to do it is to actually clone the repo:
+
+```
+hg clone https://bitbucket.org/pygame/pygame
+cd pygame
+pip3 install .
+```
--- pokemap.py
+++ pokemap.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3.3
 
+import nlzss.lzss3
+import pygame
 import struct
 import sys
 from optparse import OptionParser
@@ -24,8 +26,8 @@ def main():
   banks = find_banks(bytes, '0x3526A8')
 
   pallet_town = banks[3][0]
-  read_map(bytes, pallet_town)
-
+  (tile_splites, header_pointer) = read_map(bytes, pallet_town)
+  draw_tileset(bytes, header_pointer)
 
 def load_rom(rom_path):
   return open(rom_path, 'rb').read()
@@ -65,27 +67,87 @@ def read_map(bytes, header_pointer):
   width = read_int(bytes, map_pointer)
   height = read_int(bytes, map_pointer + 4)
   border = read_pointer(bytes, map_pointer + 8)
-  tiles = read_pointer(bytes, map_pointer + 12)
+  tiles_pointer = read_pointer(bytes, map_pointer + 12)
+  tileset_pointer = read_pointer(bytes, map_pointer + 16)
 
   debug('Map at {}'.format(map_pointer))
   debug('Width/height: {}/{}'.format(width, height))
-  debug('Border pointer: {0:#x}, tiles pointer: {1:#x}'.format(border, tiles))
+  debug('Border pointer: {0:#x}, tiles pointer: {1:#x}'.format(
+    border, tiles_pointer))
 
   tile_sprites = {}
 
+  offset = tiles_pointer
   i = 0
   for y in range(height):
     for x in range(width):
       tile_data = \
-        struct.unpack('<H', bytes[(tiles + i * 2):(tiles + (i + 1) * 2)])[0]
+        struct.unpack('<H', bytes[(offset + i * 2):(offset + (i + 1) * 2)])[0]
       attribute = tile_data >> 10
       tile = tile_data & 0x3ff
       debug('tile at ({}, {}): {:#x}, attribute: {:#x}'.format(
         x, y, tile, attribute))
-
+      tile_sprites[(x, y)] = tile
 
       i = i + 1
-  return tile_data
+
+  return (tile_sprites, tileset_pointer)
+
+def draw_tileset(bytes, tileset_pointer):
+  tileset_image_pointer = read_pointer(bytes, tileset_pointer + 4)
+  image = nlzss.lzss3.decompress_bytes(bytes[tileset_image_pointer:])
+
+  pygame.init()
+  screen = pygame.display.set_mode((300, 1000))
+  screen.fill((255, 255, 255))
+  palette = [
+    (0, 0, 0),
+    (184, 248, 136),
+    (128, 208, 96),
+    (56, 144, 48),
+    (56, 88, 16),
+    (112, 96, 96),
+    (64, 56, 48),
+    (248, 0, 248),
+    (136, 216, 184),
+    (248, 192, 112),
+    (232, 128, 104),
+    (192, 48, 64),
+    (160, 224, 192),
+    (112, 200, 160),
+    (64, 176, 136),
+    (24, 160, 104),
+  ]
+  tiles = []
+  for i in range(0, len(image), 32):
+    tile = []
+    for j in range(64):
+      px = image[int(i + (j / 2))]
+      if j % 2 == 0:
+        px = px & 0xf
+      else:
+        px = int(px / 0x10)
+      tile.append(px)
+    tiles.append(tile)
+
+  w = 2
+  for i, tile in enumerate(tiles):
+    x = (i % 16) * 16
+    y = int(i / 16) * 16
+    for j, px in enumerate(tile):
+      xx = j % 8
+      yy = int(j / 8)
+      colour = palette[px]
+      pygame.draw.rect(screen, colour,
+        (x + xx * w, y + yy * w, w, w))
+
+  debug('image length: {}'.format(len(image)))
+  pygame.display.flip()
+
+  while True:
+    event = pygame.event.wait()
+    if event.type == pygame.QUIT:
+        pygame.quit()
 
 def is_pointer(bytes, offset):
   return read_pointer(bytes, offset) > 0
